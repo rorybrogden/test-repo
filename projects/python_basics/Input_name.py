357baba1e667f4b9b37d44556ff1a7e386583930
rorybrogden <brogden.re@gmail.com>
2025-05-27 02:34:11 +0100

    Input with name and cleaning spaces, capitalizing.

---
+++ projects/python_basics/Input_name.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+
+
+#  Asking the user their name!
+name = input("What is your name?")
+
+#  Remove whitespace (beginning and end) from the string
+name = name.strip()
+
+#  Capitalize the first letter
+name = name.title()
+
+#  Print the name
+print(f"Hello, {name}!")
